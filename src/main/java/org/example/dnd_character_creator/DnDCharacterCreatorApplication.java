507480865f2332114dee53bdb0f21791072b2a1d
JakubParchem <127241475+JakubParchem@users.noreply.github.com>
2025-01-18 22:31:41 +0100

    first commit

---
+++ src/main/java/org/example/dnd_character_creator/DnDCharacterCreatorApplication.java
@@ -0,0 +1,13 @@
+package org.example.dnd_character_creator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnDCharacterCreatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DnDCharacterCreatorApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/example/dnd_character_creator/model/Race.java
@@ -0,0 +1,21 @@
+package org.example.dnd_character_creator.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Race {
+    @Id
+    private Long id;
+//
+
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
