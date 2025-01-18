6d68e33b52e2f3a8e2482b8d592bf05b3fc1a4c5
JakubParchem <jakubparchem@gmail.com>
2025-01-19 00:24:07 +0100

    added entities and linked a docker

---
+++ src/main/java/org/example/dnd_character_creator/model/Class.java
@@ -0,0 +1,61 @@
+package org.example.dnd_character_creator.model;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+public class Class {
+    @Id
+    private String id;
+
+    @Column
+    private String name;
+
+    private Short hit_die;
+
+    @OneToMany(mappedBy = "aClass")
+    private List<Subclass> subclasses;
+
+    @ManyToMany
+    private List<Spell> spells;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Short getHit_die() {
+        return hit_die;
+    }
+
+    public void setHit_die(Short hit_die) {
+        this.hit_die = hit_die;
+    }
+
+    public List<Subclass> getSubclasses() {
+        return subclasses;
+    }
+
+    public void setSubclasses(List<Subclass> subclasses) {
+        this.subclasses = subclasses;
+    }
+
+    public List<Spell> getSpells() {
+        return spells;
+    }
+
+    public void setSpells(List<Spell> spells) {
+        this.spells = spells;
+    }
+}
--- src/main/java/org/example/dnd_character_creator/model/Race.java
+++ src/main/java/org/example/dnd_character_creator/model/Race.java
@@ -1,18 +1,59 @@
 package org.example.dnd_character_creator.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import java.util.List;
 
 @Entity
 public class Race {
     @Id
-    private Long id;
+    private String id;
+    @Column
+    private String name;
 
-    public void setId(Long id) {
+    private String description;
+
+    @OneToMany(mappedBy = "race")
+    private List<Subrace> subraces;
+
+    @ManyToMany
+    private List<Trait> traits;
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
         this.id = id;
     }
 
-    public Long getId() {
-        return id;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Subrace> getSubraces() {
+        return subraces;
+    }
+
+    public void setSubraces(List<Subrace> subraces) {
+        this.subraces = subraces;
+    }
+
+    public List<Trait> getTraits() {
+        return traits;
+    }
+
+    public void setTraits(List<Trait> traits) {
+        this.traits = traits;
     }
 }
