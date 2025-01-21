8bc253bb2680b43bb0b2f0b9fd205917e8525612
JakubParchem <jakubparchem@gmail.com>
2025-01-21 23:27:09 +0100

    added frontend app

---
+++ frontend/src/main/java/org/dnd_character_creator/frontend/FrontendApplication.java
@@ -0,0 +1,14 @@
+package org.dnd_character_creator.frontend;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendApplication{
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrontendApplication.class, args);
+    }
+
+}
